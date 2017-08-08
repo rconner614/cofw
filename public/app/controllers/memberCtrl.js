01c766344688ffb7d17d6a0beee21baf6c014f29
Rachel Conner <rconner@hondros.com>
2017-08-08 08:56:27 -0400

    need image for jeanne, then done with member add

--- public/app/controllers/memberCtrl.js
+++ public/app/controllers/memberCtrl.js
@@ -2,11 +2,15 @@
     'use strict';
     angular.module('cofw.core')
         .controller('memberCtrl', memberCtrl);
-    memberCtrl.$inject = ['$scope', 'data', '$stateParams'];
-    function memberCtrl($scope, data, $stateParams){
+    memberCtrl.$inject = ['$scope', 'data', '$stateParams', '$sce'];
+    function memberCtrl($scope, data, $stateParams, $sce){
         $scope.members = data.members;
         $scope.member = $scope.members.filter(function(sItem){
             return Number(sItem.id) === Number($stateParams.id);
         })[0];
+
+        $scope.safeHTML = function (x) {
+            return $sce.trustAsHtml(x);
+        };
     }
 }());
