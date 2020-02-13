e7d2635e1e75e06541a2207d81d2799fa98d7733
Rachel Conner <rconner@hondros.com>
2020-02-13 09:59:23 -0500

    updates to members list and members display of icons

--- public/app/controllers/membersCtrl.js
+++ public/app/controllers/membersCtrl.js
@@ -2,8 +2,12 @@
     'use strict';
     angular.module('cofw.core')
         .controller('membersCtrl', membersCtrl);
-    membersCtrl.$inject = ['$scope', 'data'];
-    function membersCtrl($scope, data){
+    membersCtrl.$inject = ['$scope', 'data', '$location'];
+    function membersCtrl($scope, data, $location){
         $scope.members = data.members;
+
+        $scope.goTo = function(id) {
+            window.location.pathname = '/members/member/' + id;
+        };
     }
 }());
